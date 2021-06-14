eaf0178d5d24d81089e26e6ccb0258d9ba279470
Alessio Casolaro <alessiocasolaro@hotmail.it>
2021-06-14 18:50:59 +0200

    fix news, login

--- js/news.js
+++ js/news.js
@@ -13,47 +13,64 @@ var span1 = document.getElementById("close1");
 var span2 = document.getElementById("close2");
 var span3 = document.getElementById("close3");
 var span4 = document.getElementById("close4");
+
+var desktop = document.getElementById("desktop");
+var desktop2 = document.getElementById("desktop2");
+var news = document.getElementById("newsDiv");
+var news2 = document.getElementById("newsDiv2");
 // When the user clicks the button, open the modal 
 
+window.onresize = whatDevice;
+function whatDevice() {
+  if (window.innerWidth < 1300) {
+    desktop.remove();
+    desktop2.remove();
+  }
+  else
+    if (window.innerWidth > 1300) {
+      news.appendChild(desktop);
+      news2.appendChild(desktop2);
+    }
+}
 
-btn.onclick = function() {
+btn.onclick = function () {
   modal1.style.display = "block";
 }
 
-btn2.onclick = function() {
-    modal2.style.display = "block";
+btn2.onclick = function () {
+  modal2.style.display = "block";
 }
 
-btn3.onclick = function() {
+btn3.onclick = function () {
   modal3.style.display = "block";
 }
 
-btn4.onclick = function() {
+btn4.onclick = function () {
   modal4.style.display = "block";
 }
 
 
 // When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-    modal1.style.display = "none"; 
+span1.onclick = function () {
+  modal1.style.display = "none";
 }
 
-span2.onclick = function() {
-    modal2.style.display = "none";
+span2.onclick = function () {
+  modal2.style.display = "none";
 }
 
-span3.onclick = function() {
+span3.onclick = function () {
   modal3.style.display = "none";
 }
 
-span4.onclick = function() {
+span4.onclick = function () {
   modal4.style.display = "none";
 }
 
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal1 || event.target == modal2|| event.target == modal3|| event.target == modal4) {
+window.onclick = function (event) {
+  if (event.target == modal1 || event.target == modal2 || event.target == modal3 || event.target == modal4) {
     modal1.style.display = "none";
     modal2.style.display = "none";
     modal3.style.display = "none";
